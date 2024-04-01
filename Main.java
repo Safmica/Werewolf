aae19b000db1bbdaa5c32fc39d48f31aea02e031
Safmica <safmikasa@gmail.com>
2024-04-01 12:52:39 +0700

    feat: add status player, add some win conditions and add new UI fix: werewolf bug

--- Main.java
+++ Main.java
@@ -5,18 +5,30 @@ public class Main{
     static Random ran = new Random();
     public static void main(String[] args) {
         boolean status = true;
+        boolean play = true;
         while(status){
             String role[] = {"Villager","Villager","Villager","Werewolf","Werewolf","Sorcerer","Seer","Doctor","Ladybug","Bomberman","Traitor","Dopplegangger","Joker"};
             String player[][] = new String[13][2];
 
-            shuffleArray(role);
-            inputPlayer(role, player);
-            showRole(player);
+            ShuffleArray(role);
+            InputPlayer(role, player);
+            System.out.println();
+            System.out.println("ROLE WEREWOLF");
+            System.out.println();
+            ShowArray(player);
+            play = true;
+            while (play) {
+                play = Menu(player,play);
+                ShowArray(player);
+            }
+            System.out.println();
+            System.out.println( "WEREWOLF GAME");
+            System.out.println("================");
         }
         scr.close();
     }
 
-    static void inputPlayer(String role[], String player[][]){
+    static void InputPlayer(String role[], String player[][]){
         for(int i = 0;i<role.length;i++){
             int a = 0;
             int b = 1;
@@ -26,7 +38,7 @@ public class Main{
         }
     }
 
-    static void shuffleArray(String[] array) {
+    static void ShuffleArray(String[] array) {
         Random rnd = new Random();
         for (int i = array.length - 1; i > 0; i--) {
             int index = rnd.nextInt(i + 1);
@@ -37,7 +49,7 @@ public class Main{
         }
     }
 
-    static void showRole(String player[][]){
+    static void ShowArray(String player[][]){
         for(int i = 0;i<player.length;i++){
             System.out.print((i+1)+". ");
             System.out.print(player[i][0]);
@@ -46,5 +58,116 @@ public class Main{
             System.out.print(")");
             System.out.println();
         }
+    }
+
+    static boolean Menu(String player[][],boolean play){
+        System.out.println();
+        System.out.println("Moderator Menu");
+        System.out.println("1. Werewolf");
+        System.out.println("2. Seer");
+        System.out.println("3. Doctor");
+        System.out.println("4. Sorcerer");
+        System.out.println("5. Ladybug");
+        System.out.println("6. Bomberman");
+        System.out.println("7. Dopplegangger (vote 1)");
+        System.out.println("8. Vote");
+        System.out.println();
+        System.out.print("Input = ");
+        int input = scr.nextInt();
+        scr.nextLine();
+        String target;
+
+        switch (input) {
+            case 1:
+                System.out.print("Target = ");
+                target = scr.nextLine();
+                Werewolf.Kill(player, target);
+                System.out.println();
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                
+                break;
+            default:
+                break;
+        }
+        
+        //check werewolf and traitor status
+        int ww = SequentialSearch(player, "Werewolf");
+        int sorcerer = SequentialSearch(player, "Sorcerer");
+        int traitor = SequentialSearch(player, "Traitor");
+        if(ww == -1 && sorcerer == -1){
+            if(traitor != -1){
+                System.out.println("====|Congratulations "+player[traitor][0]+", Traitor WIN|====");
+                play = false;
+                return play;
+            }
+            else{
+                System.out.println("====|Congratulations Villagers WIN|====");
+                play = false;
+                return play;
+            }
+        }
+        else if(ww != -1 || sorcerer != -1){
+            int bad = 0;
+            int good = 0;
+            for(int i = 0; i <player.length; i++){
+                if(player[i][1].equals("Werewolf") || player[i][1].equals("Sorcerer")) bad += 1;
+                if(!player[i][1].equals("Werewolf") && !player[i][1].equals("Sorcerer") && !player[i][1].equals("Death")) good += 1;
+            }
+            if(good == bad){
+                System.out.println("====|Werewolf and Sorcerer WIN|====");
+                play = false;
+                return play;
+            }
+            System.out.println("====|"+Count(player, "Werewolf")+" Werewolf left and "+Count(player, "Sorcerer")+" Sorcerer left"+"|====");
+        }
+        else{
+            System.out.println("====|"+Count(player, "Werewolf")+" Werewolf left and "+Count(player, "Sorcerer")+" Sorcerer left"+"|====");
+        }
+
+        return play;
+    }
+
+    static int SequentialSearch(String[][] array, String target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][1].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int TargetSearch(String[][] array, String target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][0].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int Count(String[][] array, String target){
+        int count = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i][1].equals(target)){
+                count+=1;
+            }
+        }
+        return count;
     }
 }
--- Werewolf.java
+++ Werewolf.java
@@ -1,5 +1,13 @@
 public class Werewolf extends Role{
+    String target;
+    
     public Werewolf(){
         super("Werewolf");
+    }
+
+    public static void Kill(String[][] player, String name){
+        int x = Main.TargetSearch(player, name);
+        player[x][0] = "Death";
+        player[x][1] = "Death";
     }
 }
